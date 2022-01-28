a141d198ae5c9110f66c3f044a2ead62381e4638
Luis Gabriel N. Simas <gabriel.simas@thera.com.br>
2022-01-27 22:55:26 -0300

    Node.Js MongoDB Authentication

--- src/api/index.js
+++ src/api/index.js
@@ -2,14 +2,17 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 require('dotenv').config();
-const userRoute = require('./routes/users')
+const userRoute = require('./routes/users');
+const authRoute = require('./routes/auth');
 
 mongoose.connect(process.env.MONGO_URL)
     .then(() => console.log('DBConnection Succesfull!'))
     .catch((err) => console.error(err));
 
 app.use(express.json());
+app.use('/api/v1/auth',authRoute);
 app.use('/api/v1/user',userRoute);
+
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("Backend server is running!");
