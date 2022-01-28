1b336ccd4c25fcee0f6972e55cefaeb01a28c715
Luis Gabriel N. Simas <gabriel.simas@thera.com.br>
2022-01-27 23:08:48 -0300

    Node.js JWT Implementation

--- src/api/routes/auth.js
+++ src/api/routes/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const User = require('../models/User');
 const CryptoJS = require('crypto-js');
+const jwt = require('jsonwebtoken');
 
 // REGISTER
 router.post('', async (req, res) => {
@@ -37,9 +38,14 @@ router.post('/login', async (req, res) => {
         originalPassword !== req.body.password &&
             res.status(401).json("Wrong Credentials!");  
             
+            //TODO: Create a collection that record the generated token by user for the case this token is valid!
+            const accessToken = jwt.sign({
+                id:user._id,isAdmin: user.isAdmin,
+            }, process.env.JWT_SECRET,{expiresIn:"3d"});
+            
             const {password, ...others} = user._doc;
 
-        res.status(200).json(others);
+        res.status(200).json({...others,accessToken});
     } catch (err) {
         res.status(500).json(err);
     }
